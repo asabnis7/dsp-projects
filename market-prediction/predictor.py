08bd8105b0b280bc1e8a005b0662eb998eb2d04a
Arjun Sabnis <43247959+asabnis7@users.noreply.github.com>
2019-06-29 05:00:12 +0530

    Add files via upload

--- market-prediction/predictor.py
+++ market-prediction/predictor.py
@@ -1,3 +1,6 @@
+# Arjun Sabnis
+# Market predictor algorithm
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,24 +10,24 @@ aapl_raw = aapl_raw[::-1];
 aapl_df = aapl_raw.loc[:,('Date','Adj. Close')];
 aapl_np = aapl_df.loc[:,'Adj. Close'].to_numpy();
 
-start = 9000
-end = 9397
+start = 9200
+end = 9398
 
 aapl_close = aapl_np[start:end].reshape(end-start,1);
 
-Y = len(aapl_close);
-p = 20;
+N = len(aapl_close);
+p = 40;
 
-X = np.zeros((Y-p, p));
-x = np.zeros((Y-p, 1));
+X = np.zeros((N-p, p));
+x = np.zeros((N-p, 1));
 
 year = 1;
 
-for i in range(0, Y-p):
-    for j in range(0, p):
-        X[i,j] = aapl_close[i+j-1];
+for i in range(0,N-p):
+    for j in range(0,p):
+        X[i,j] = aapl_close[i+j];
 
-    x[i] = aapl_close[i+j];
+    x[i] = aapl_close[i+j+1];
     
     if (i > year*p):
         a = np.matmul(-np.linalg.pinv(X), x);
@@ -35,18 +38,21 @@ xhat = np.matmul(-X, a);
 aapl_new = aapl_close;
 xhat_new = xhat;
 
-for c in range(0,3):
-    for i in range(0, Y-p):
-        for j in range(0,p):
-            X[i,j] = aapl_new[i+j+c];
-    
-    xhat = np.matmul(-X,a);
-    xhat_new = np.append(xhat_new, xhat[-1,:]);
-    #aapl_new = np.append(aapl_new, aapl_np[end+1+c]);
-    aapl_new = np.append(aapl_new, xhat[-1,:]);
+#for i in range(0,N-p):
+#    for j in range(0,p):
+#        X[i,j] = aapl_new[i+j+c];
+#xhat = np.matmul(-X,a);
+#xhat_new = np.append(xhat_new, xhat[-1,:]);
+#aapl_new = np.append(aapl_new, xhat[-1,:]);
+#aapl_new = np.append(aapl_new, aapl_np[end+1+c]);
+
+for c in range(0,5):
+    xhat = -np.dot(a.T, np.flip(aapl_new[N-p:N]))
+    xhat_new = np.concatenate((xhat_new, xhat));
+    aapl_new = np.concatenate((aapl_new, xhat));
+    N = len(aapl_new);
 
 plt.plot(xhat_new, label='Prediction');
-plt.plot(aapl_df.iloc[start+p-2:end].loc[:,'Adj. Close'].to_numpy(), label='AAPL Stock');
-#plt.plot(aapl_new[p-c:-3], label='AAPL Stock');
+plt.plot(aapl_df.iloc[start+p-1:end].loc[:,'Adj. Close'].to_numpy(), label='AAPL Data');
 plt.legend();
 plt.show();
